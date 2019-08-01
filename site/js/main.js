07e9d7b8e5b5a1556f951e2c7b1eb8eeecbd41ad
Gaibullaev Islam <Gaibullaeviz@gmail.com>
2019-08-01 13:52:17 +0300

    start

---
+++ site/js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+  $('body').append('<button class="btn" />');
+  $('.btn').click(function(){
+    $('body').animate({'scrollTop': 1286}, 3000);
+    $('html').animate({'scrollTop': 1286}, 3000);
+});
+});
+$(function(){
+  $('body').append('<button class="btn1" />');
+  $('.btn1').click(function(){
+    $('body').animate({'scrollTop': 0}, 3000);
+    $('html').animate({'scrollTop': 0}, 3000);
+});
+});
+$(window).scroll(function(){
+
+  var st = $(this).scrollTop();
+  console.log(st);
+
+  $(".p2").css({
+    "transform" : "translate(0%, -" + st/5 + "%"
+  });
+  $(".p1").css({
+    "transform" : "translate(0%, -" + st/20 + "%"
+  });
+});
